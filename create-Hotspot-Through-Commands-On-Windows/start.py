340cbfe2ca1779576b370b1ab35e6da10a26a341
atulac <chaturvediatul05@gmail.com>
2017-05-14 13:19:00 +0530

    Commit 2

--- create-Hotspot-Through-Commands-On-Windows/start.py
+++ create-Hotspot-Through-Commands-On-Windows/start.py
@@ -1,9 +1,9 @@
-#Snippet to start hotspot from command prompt
+"""Snippet to start hotspot from command prompt"""
 
 from subprocess import call,check_output
-call('netsh wlan set hostednetwork mode=allow ssid=YourHotspotName key=YourPassword',shell=True)
+call('netsh wlan set hostednetwork mode=allow ssid=MyHotspot key=getconnected',shell=True)
 
-#ssid and key to be put as per one's own interest
+#ssid and key to be put as per one's own choice
 
 call('netsh wlan start hostednetwork',shell=True)
 raw_input("press enter to exit ;)")
